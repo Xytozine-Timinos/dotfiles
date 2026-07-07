cbb5569e4b63c3beb31058803fed0e9a93df5b2a
Xytozine <lemonadepham@gmail.com>
2026-07-07 12:30:22 +0700

    update nvim treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	commit = "42fc28b",
+	commit = "main",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
+		require("nvim-treesitter").setup({
 			ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 			auto_install = true,
 			highlight = { enable = true },
