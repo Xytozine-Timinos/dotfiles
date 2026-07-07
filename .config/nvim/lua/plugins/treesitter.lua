87922edb40bbf851d0e259a99069c38e175284f4
Xytozine <lemonadepham@gmail.com>
2026-07-07 12:37:17 +0700

    update nvim treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,11 +3,30 @@ return {
 	commit = "main",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter").setup({
-			ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-			auto_install = true,
+		local treesitter = require("nvim-treesitter")
+
+		treesitter.setup({
 			highlight = { enable = true },
 			indent = { enable = true },
+		})
+
+		treesitter.install({ "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(ev)
+				local lang = vim.treesitter.language.get_lang(ev.match)
+				local available_langs = treesitter.get_available()
+				local is_available = vim.tbl_contains(available_langs, lang)
+				if is_available then
+					local installed_langs = require("nvim-treesitter").get_installed()
+					local installed = vim.tbl_contains(installed_langs, lang)
+					if not installed then
+						require("nvim-treesitter").install(lang):wait()
+					end
+					vim.treesitter.start()
+					require("nvim-treesitter").indentexpr()
+				end
+			end,
 		})
 	end,
 }
