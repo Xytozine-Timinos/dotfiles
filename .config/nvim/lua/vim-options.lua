96f1c09b756af18ec0114d1c25f5b38d22bba2ee
Xytozine <lemonadepham@gmail.com>
2026-07-07 10:12:23 +0700

    remove the keymap
    fuckass

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -24,8 +24,6 @@ km.set("n", "<C-Space>", "<Esc>", { noremap = true, silent = true })
 km.set("i", "<C-Space>", "<Esc>", { noremap = true, silent = true })
 km.set("v", "<C-Space>", "<Esc>", { noremap = true, silent = true })
 
-vim.keymap.set('i', '<M-f>', '<Esc>', { desc = 'Exit insert mode' })
-
 km.set("n", "<leader>SO", "<cmd>source %<CR>")
 km.set("n", "<leader>l", "<cmd>Lazy<CR>")
 km.set("n", "<leader>m", "<cmd>Mason<CR>")
