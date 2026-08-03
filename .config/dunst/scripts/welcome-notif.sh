53516fd123d9dcc794bca17549d8ef6e84da50a9
Xytozine <azullemonadepham@gmail.com>
2026-08-03 23:06:25 +0700

    update welcome notification

--- .config/dunst/scripts/welcome-notif.sh
+++ .config/dunst/scripts/welcome-notif.sh
@@ -5,6 +5,6 @@ source ~/.config/dtf-config/config
 welcome_notification=${welcome_notification:-false}
 
 if [[ $welcome_notification == "true" ]]; then
-	notify-send -t 5000 "Dotfiles V4" "Welcome back $(whoami)!"
+	notify-send -t 5000 "Azure Delta+ Desktop" "Welcome back $(whoami)!"
 	paplay $CUSTOM_SOUND_PATH/desktop-login.oga
 fi
