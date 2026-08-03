dba8a3714cfc407cba2d86cdf9133d3995e33b85
Xytozine <azullemonadepham@gmail.com>
2026-08-03 23:14:34 +0700

    ...

--- .config/waybar/scripts/right_click_window.sh
+++ .config/waybar/scripts/right_click_window.sh
@@ -49,7 +49,7 @@ $hide_window)
 	~/.config/hypr/scripts/hide_unhide_window.sh h
 	;;
 $float_window)
-	hyprctl dispatch togglefloating && hyprctl dispatch resizeactive exact 70% 70%
+	# hyprctl dispatch togglefloating && hyprctl dispatch resizeactive exact 70% 70%
 	hyprctl eval '
 	    hl.dispatch(hl.dsp.window.float({ action = "toggle" }))
 	    local monitor = hl.get_active_monitor()
