bcd2d109a3a595a787c9a6ba6e4910950535b612
Xytozine <lemonadepham@gmail.com>
2026-09-05 13:55:54 +0700

    update hyprland wm keybinds
    added alt + left click to resize window

--- .config/hypr/hyprland_config_modules/keybinds/wm_keybinds.lua
+++ .config/hypr/hyprland_config_modules/keybinds/wm_keybinds.lua
@@ -10,6 +10,7 @@ hl.bind(mod .. " + SHIFT + r", hl.dsp.exec_cmd('notify-send "Hyprland Manually R
 
 -- Move/resize windows with mainMod + LMB/RMB and dragging
 hl.bind(mod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
+hl.bind(alt .. " + mouse:272", hl.dsp.window.resize(), { mouse = true })
 hl.bind(mod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Lock screen via hyprlock
