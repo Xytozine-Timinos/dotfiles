49836b609ff85cd4ae7bee6bc3f8afb0afadd32f
Xytozine <lemonadepham@gmail.com>
2026-09-06 10:32:32 +0700

    removed duplicated

--- .config/hypr/hyprland_config_modules/exec.lua
+++ .config/hypr/hyprland_config_modules/exec.lua
@@ -12,8 +12,6 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 24")
 	-- Polkit agent, required for graphical apps
 	hl.exec_cmd("systemctl --user start hyprpolkitagent")
-	-- Waybar
-	hl.exec_cmd(home .. "/.config/waybar/startup.sh")
 	-- Hyprsunset
 	hl.exec_cmd(home .. "/.config/hypr/scripts/hyprsunset_launch.sh")
 	-- Animations option on/off
